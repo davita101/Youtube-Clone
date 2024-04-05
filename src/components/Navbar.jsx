c69f1611c839321c3ba3ce1ce06c15646409904a
davita101 <chemsfexs@gmail.com>
2024-04-06 03:22:19 +0400

    Add constants, finish NavBar sratch, add extra pacages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { SearchBar } from './index.js'
+import { logo } from '../utils/constants.jsx'
 
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
-}
+const Navbar = () => (
+  <Stack 
+  direction='row'
+  alignItems='center'
+  p={2} className=' sticky bg-black top-0 justify-between'>
+    <Link to='/' className='flex items-center' >
+      <img src={logo} alt='logo' className='h-[45px]' />
+    </Link>
+    <SearchBar/>
+  </Stack>
+)
 
 export default Navbar
---
+++ src/components/SearchBarr.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useNavigate} from 'react-router-dom'
+import { Paper, IconButton } from '@mui/material'
+import Search from '@mui/icons-material/Search';
+
+const SearchBarr = () => {
+  return (
+    <Paper
+      component='form'
+      onSubmit={() => {}}
+      sx={{
+        borderRadius:20,
+        border: '1px solid black',
+        pl: 2,
+        boxShadow:'none',
+        mr: {sm: 5}
+      }}
+      
+    >
+      <input 
+        className='search-bar'
+        placeholder='Search...'
+        value=''
+        onChange={() => {}}
+       />
+       <IconButton type='submit' sx={{
+           p: '10px',
+           color: 'red'
+           }}>
+        <Search />
+       </IconButton>
+    </Paper>
+  )
+}
+
+export default SearchBarr
